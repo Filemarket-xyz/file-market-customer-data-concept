11a97a61f6f1f5e4e5f24284a68b01cac0f6e749
alik <colinmilton1@gmail.com>
2024-05-23 23:21:39 +0300

    ConnectButton added

---
+++ frontend/src/features/Connect/ui/ConnectButton/ConnectButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useActiveAccount, useConnect, useDisconnect } from 'thirdweb/react'
+import { createWallet } from 'thirdweb/wallets'
+import { useSignMessage } from 'wagmi'
+
+import { client } from '~/shared/lib'
+import { Button } from '~/shared/ui'
+
+const metamask = createWallet("io.metamask")
+
+export const ConnectButton: React.FC = () => {
+  const { connect, isConnecting } = useConnect()
+  const { disconnect } = useDisconnect()
+  const { signMessageAsync } = useSignMessage()
+  const account = useActiveAccount()
+
+  const onPress = () => {
+    if (account) {
+      disconnect(metamask)
+    } else {
+      connect(async() => {
+        await metamask.connect({ client })
+
+        await signMessageAsync({ message: 'Hello, I am using WalletConnect' })
+
+        return metamask
+      })
+    }
+  }
+
+  return (
+    <Button
+      variant='secondary'
+      size="small"
+      loading={isConnecting}
+      onPress={onPress}
+    >
+      {account ? 'Disconnect' : 'Connect'}
+    </Button>
+  )
+}
