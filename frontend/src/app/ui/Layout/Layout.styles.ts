31e5571f9173394b8a6be5e43dbef11c3a0fad8c
alik <colinmilton1@gmail.com>
2024-05-26 19:35:35 +0300

    Layout fixes

--- frontend/src/app/ui/Layout/Layout.styles.ts
+++ frontend/src/app/ui/Layout/Layout.styles.ts
@@ -1,15 +1,12 @@
 import { styled } from '~/shared/styles'
 
-export const StyledLayout = styled('main', {
-  display: 'flex',
-  flexDirection: 'column',
+export const StyledLayout = styled('div', {
   width: '100%',
   height: '100%',
 })
 
-export const StyledBody = styled('div', {
+export const StyledBody = styled('main', {
   paddingTop: '$sizes$header',
   width: '100%',
-  height: '100svh',
   minHeight: '100svh',
 })
